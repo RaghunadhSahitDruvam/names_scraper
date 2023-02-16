dec6aea1a0d096eb6354e05b0af4abaedaed3f1d
Raghunadh <konidoniravirr@gmail.com>
2023-02-16 07:53:27 +0530

    names_scraper

---
+++ .history/index_20230215185643.js
@@ -0,0 +1,47 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+const express = require("express");
+const cors = require("cors");
+// require executablePath from puppeteer
+const { executablePath } = require("puppeteer");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const app = express();
+
+app.use(cors());
+let data = [];
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: executablePath(),
+  });
+  const page = await browser.newPage();
+
+  //TODO:  Motilal Oswal, nify and s and t corporation unte remove chey
+  await page.goto("https://indianhindunames.com/indian-hindu-girl-name-a.htm");
+  for (let i = 1; i < 494; i++) {
+    await page.waitForSelector(
+      `body > div:nth-child(6) > div > div.col-lg-10 > div > div.col-lg-7.nl > ul > li:nth-child(${i})`
+    );
+    let element = await page.$(
+      `body > div:nth-child(6) > div > div.col-lg-10 > div > div.col-lg-7.nl > ul > li:nth-child(${i})`
+    );
+    let value = await page.evaluate((el) => el.textContent, element);
+
+    let fetchUrl = `https://zeppoh.com/convert?name=${value.split("=", 2)[0]}`;
+    fetch(fetchUrl)
+      .then((res) => res.json())
+      .then((d) => data.push(d));
+    console.log(data);
+    await page.goBack();
+  }
+
+  await browser.close();
+})();
+app.get("/", function (req, res) {
+  res.status(200).json(data);
+});
+app.listen(5173, () => {
+  console.log("Yes iam running on port 5173");
+});
---
+++ .history/index_20230215190214.js
@@ -0,0 +1,47 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+const express = require("express");
+const cors = require("cors");
+// require executablePath from puppeteer
+const { executablePath } = require("puppeteer");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const app = express();
+
+app.use(cors());
+let data = [];
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: executablePath(),
+  });
+  const page = await browser.newPage();
+
+  await page.goto("https://indianhindunames.com/indian-hindu-girl-name-a.htm");
+  for (let i = 1; i < 3; i++) {
+    await page.waitForSelector(
+      `body > div:nth-child(6) > div > div.col-lg-10 > div > div.col-lg-7.nl > ul > li:nth-child(${i})`
+    );
+    let element = await page.$(
+      `body > div:nth-child(6) > div > div.col-lg-10 > div > div.col-lg-7.nl > ul > li:nth-child(${i})`
+    );
+    let value = await page.evaluate((el) => el.textContent, element);
+    // value = value.split("=", 1)[0];
+
+    // let fetchUrl = `https://zeppoh.com/convert?name=${value}`;
+    // fetch(fetchUrl)
+    //   .then((res) => res.json())
+    //   .then((d) => data.push(d));
+    // console.log(data);
+    console.log(value);
+  }
+
+  await browser.close();
+})();
+app.get("/", function (req, res) {
+  res.status(200).json(data);
+});
+app.listen(5173, () => {
+  console.log("Yes iam running on port 5173");
+});
