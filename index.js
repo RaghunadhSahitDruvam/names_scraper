f6f9687e1b556cd22459657ccc1cf638b9b55d90
Raghunadh <konidoniravirr@gmail.com>
2023-06-19 10:17:50 +0530

    Yes

--- index.js
+++ index.js
@@ -1,89 +1,3 @@
-// const puppeteer = require("puppeteer-extra");
-// const express = require("express");
-// const cors = require("cors");
-// const axios = require("axios");
-// // require executablePath from puppeteer
-// const { executablePath } = require("puppeteer");
-// const fetch = (...args) =>
-//   import("node-fetch").then(({ default: fetch }) => fetch(...args));
-// const app = express();
-
-// app.use(cors());
-// let data = [];
-// (async () => {
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     executablePath: executablePath(),
-//   });
-//   const page = await browser.newPage();
-//   await page.goto(
-//     "https://6481bb4dd1bc5e161f9ff130--soft-capybara-d1c636.netlify.app/"
-//   );
-//   //29759
-//   //14880
-//   //7440
-//   for (let i = 1; i < 4031; i++) {
-//     // #root > div > tr:nth-child(200)
-//     await page.waitForSelector(`#root > div > tr:nth-child(${i})`);
-//     // await page.setDefaultNavigationTimeout(0);
-
-//     let element = await page.$(`#root > div > tr:nth-child(${i})`);
-//     let value = await page.evaluate((el) => el.textContent, element);
-//     value = value.split("=", 1)[0];
-//     value = value.split("/", 1)[0];
-//     value = value.split(",", 1)[0];
-//     value = value.split("'", 1)[0];
-//     value = value.split("(", 1)[0];
-//     value = value.split("-", 1)[0];
-
-//     value = value
-//       .replaceAll("Génîn", "genin")
-//       .replaceAll("Bríet", "bhuja")
-//       .replaceAll("Björg", "bhoja")
-//       .replaceAll("Björk", "bindu")
-//       .replaceAll("Elizé", "elize")
-//       .replaceAll("Êrsta", "hj")
-
-//       .replaceAll("SharÃ»", "sharda")
-//       .replaceAll("SinadÃ©vi", "Saindhavi")
-//       .replaceAll("DakshayajÃ±avinaashin", "Dakshayajvinaashin")
-//       .replaceAll("-", "")
-//       .replaceAll(".", " ")
-//       .replaceAll("{", "")
-//       .replaceAll("}", "")
-//       .replaceAll("( ", "")
-//       .replaceAll(" )", "")
-//       .replaceAll("(", "")
-//       .replaceAll(")", "")
-//       .replaceAll("  ", " ")
-//       .replaceAll("'", "")
-//       .replaceAll("&", "and")
-//       .replaceAll("3", "c")
-//       .replaceAll(`"`, "")
-//       .replaceAll(`  `, "")
-//       .replaceAll("2", "b")
-//       .replaceAll("1", "a")
-//       .replaceAll("LTD", "")
-//       .replaceAll("Ltd", "")
-//       .replaceAll("ltd", "")
-//       .replaceAll("Limited", "")
-//       .replaceAll("LIMITED", "");
-
-//     fetch(`https://zeppoh.com/convert?name=${value}`)
-//       .then((res) => res.json())
-//       .then((d) => data.push(d));
-//     console.log(data);
-//   }
-
-//   await browser.close();
-// })();
-// app.get("/", function (req, res) {
-//   res.status(200).json(data);
-// });
-// app.listen(5173, () => {
-//   console.log("Yes iam running on port 5173");
-// });
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -253,6 +167,32 @@ app.get("/sixor7girlNames", (req, res) => {
     root: path.join(__dirname),
   };
   const fileName = "sixor7girlNames.json";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
+app.get("/bulkBoyData", (req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  const fileName = "bulkBoyData.json";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
+app.get("/only5letterBoynames", (req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  const fileName = "only5letterBoynames.json";
   res.sendFile(fileName, options, function (err) {
     if (err) {
       next(err);
